126c5ef00137629ad4276d9da3d2ab9a20cc2098
danielaA01367611 <A01367611@tec.mx>
2024-04-17 19:35:57 -0600

    grafica

--- hola.py
+++ hola.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title('Titanic App')
 st.header('Titanic Graphs')
@@ -9,3 +10,8 @@ st.write('Graficas del dataset titanic')
 titanic_link='titanic.csv'
 titanic_data=pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots() 
+ax.hist(titanic_data['Fare']) 
+st.header("Histograma del Titanic") 
+st.pyplot(fig)
