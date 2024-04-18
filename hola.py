0cf87a54da00c193160198bda978ffe9a0319f35
danielaA01367611 <A01367611@tec.mx>
2024-04-17 18:34:00 -0600

    parte 1

---
+++ hola.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Titanic App')
+st.header('Demo titanic App')
+st.write('Graficas del dataset titanic')
+
+
+titanic_link='titanic.csv'
+titanic_data=pd.read_csv(titanic_link)
+st.dataframe(titanic_data)
